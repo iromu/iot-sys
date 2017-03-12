383e2964c4e0c950b4e7d8abc491e6e4d456a793
Iván Rodríguez Murillo <wantez@gmail.com>
2017-03-13 00:16:09 +0100

    portainer docker

---
+++ docker/portainer/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+eval $(docker-machine env blackbox)
+
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    portainer/portainer \
+    -H unix:///var/run/docker.sock
---
+++ docker/portainer/production.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+eval $(docker-machine env blackbox)
+
+docker-compose -f docker-compose.yml up
+
